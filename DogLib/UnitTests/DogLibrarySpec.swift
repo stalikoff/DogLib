793d73766f29f5357b2d8489d15f25a1d4773df3
Oleg Vasilev <o.vasilev@danycom.ru>
2022-12-26 03:51:41 +0400

    added tests

--- DogLib/UnitTests/DogLibrarySpec.swift
+++ DogLib/UnitTests/DogLibrarySpec.swift
@@ -7,51 +7,59 @@
 @testable import DogLib
 import Quick
 import Nimble
-//import quic
 
 class DogLibrarySpec: QuickSpec {
     override func spec() {
-        describe("Subtraction") {
+        describe("DogLibrary") {
             var library: DogLibrary!
 
             beforeEach {
                 library = DogLibrary()
-                library.getNextImage { image in
+            }
 
+            describe("Testing next previous methods") {
+                var result: UIImage?
+
+                it("next image not nil") {
+                    library.getNextImage { image in
+                        result = image
+                    }
+
+                    expect(result).toEventuallyNot(beNil(),
+                                                timeout: DispatchTimeInterval.seconds(5))
+                }
+
+                it("previous image nil") {
+                    library.getPreviousImage { image, isFirst in
+                        result = image
+                    }
+
+                    expect(result).toEventually(beNil(),
+                                                timeout: DispatchTimeInterval.seconds(1))
                 }
             }
 
-            it("test next image") {
-                var imageTest: UIImage?
+            describe("Testing load images") {
+                var imagesCount = 0
 
-                library.getPreviousImage { image, isPrevious in
-                    imageTest = image
+                it("test get one image") {
+                    library.getImage { imageData in
+                        if imageData != nil {
+                            imagesCount = 1
+                        }
+                    }
+                    expect(imagesCount).toEventually(equal(1),
+                                                timeout: DispatchTimeInterval.seconds(5))
                 }
 
-                it("previous image should not be nil") {
-                    expect(imageTest != nil)
+                it("test get 3 images") {
+                    library.getImages(number: 3, completion: { images in
+                        imagesCount = images?.count ?? 0
+                    })
+                    expect(imagesCount).toEventually(equal(3),
+                                                timeout: DispatchTimeInterval.seconds(5))
                 }
-
-
-//                library.getNextImage { <#UIImage?#> in
-//                    <#code#>
-//                }
             }
-
-
-
-
-//            it("can subtract one number from the first one") {
-//                expect(library.getNextImage(completion: { <#UIImage?#> in
-//                    <#code#>
-//                }))
-//                expect(subtraction.subtract(number1: 3, number2: 2)) == 1
-//            }
         }
     }
-
-    func testStart() {
-        print("")
-    }
-
 }
